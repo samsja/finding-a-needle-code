251976f815513de9494bc2c691c7720936b19db3
Sami Jaghouar <sami.jaghouar@zenseact.com>
2021-02-17 11:31:22 +0100

    rearrange path
    Change-Id: I1041be387cdba91ce4b5078ae3aacc557c0c0ca6

--- patch_extraction.py
+++ patch_extraction.py
@@ -4,35 +4,38 @@ from PIL import Image
 import os
 
 
+sub_path = "converted"
 
+data_path="traffic-signs"
 
-img_path = "traffic-signs/images/"
-annot_path = "traffic-signs/annotations/"
+img_path = f"{data_path}/{sub_path}/images/"
+annot_path = f"{data_path}/{sub_path}/annotations/"
+output_path = f"{data_path}/patched"
 
+for json_ in os.listdir(annot_path):
 
-for json_ in os.listdir(annot_path)[59:]:
-    
     if "json" not in json_:
         continue
-    
-    with open(annot_path+json_) as json_file:
+    with open(annot_path + json_) as json_file:
         data = json.load(json_file)
 
         fn = data["Scenes"]["1"]["Sensors"]["FC"]["Filename"]
 
         try:
-            img = np.array(Image.open(img_path+fn))
+            img = np.array(Image.open(img_path + fn))
         except:
             continue
 
         signs_ = data["Scenes"]["1"]["TrafficSigns"]
-      
 
         for o in signs_:
-            if "NotListed" not in signs_[o]["2dMarking"]["FC"]["SignProperties"]["Type"]:
-                
+            if (
+                "NotListed"
+                not in signs_[o]["2dMarking"]["FC"]["SignProperties"]["Type"]
+            ):
+
                 class_ = signs_[o]["2dMarking"]["FC"]["SignProperties"]["Type"]
-                
+
                 x1 = int(signs_[o]["2dMarking"]["FC"]["Top"]["X"])
                 x2 = int(signs_[o]["2dMarking"]["FC"]["Bottom"]["X"])
 
@@ -40,10 +43,8 @@ for json_ in os.listdir(annot_path)[59:]:
                 y2 = int(signs_[o]["2dMarking"]["FC"]["Bottom"]["Y"])
 
                 patch = img[y1:y2, x1:x2]
-                
-                
-                if not os.path.isdir(class_):
-                    os.mkdir(class_)
 
-                Image.fromarray(patch).save(class_+"/"+o+".jpg")
-      
+                if not os.path.isdir(f"{output_path}/{class_}"):
+                    os.makedirs(f"{output_path}/{class_}")
+
+                Image.fromarray(patch).save(f"{output_path}/{class_}/{o}.jpg")
