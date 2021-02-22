38d08b9661b5c045c50f1399a74bbf1604080c3a
Sami Jaghouar <sami.jaghouar@zenseact.com>
2021-02-22 09:22:09 +0100

    modify the .gitignore to avoid commiting cpython cache add the FewShotDataSet and the FewShotSampler modify the omniglot dataset from torchvision to a FewShotDataSet
    Change-Id: I5e320cc3ec2927f53cf0c18d4284ff01c250253d

---
+++ src/few_shot_learning/omniglot.py
@@ -0,0 +1,26 @@
+import torch
+import torchvision
+from data_handler import FewShotDataSet
+
+
+class Omniglot(torchvision.datasets.Omniglot, FewShotDataSet):
+    def __init__(self, *args, **kwargs):
+        super(Omniglot, self).__init__(*args, **kwargs)
+
+        self.classes: torch.Tensor[str] = torch.Tensor(
+            [label for (image_fn, label) in self._flat_character_images]
+        ).unique()
+        self.length_of_class = len(self._character_images[0])
+
+    def get_index_in_class(self, class_idx: int):
+        """
+        Method to get the indexes of the elements in the same class as class_idx
+
+        # Args:
+            class_idx : int. The index of the desider class
+
+        """
+        return torch.arange(
+            class_idx * self.length_of_class, (class_idx + 1) * self.length_of_class
+        )
+
