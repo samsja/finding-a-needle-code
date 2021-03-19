48677c94965e8da2fd9722365bea65a9a2cfe67f
Sami Jaghouar <sami.jaghouar@zenseact.com>
2021-03-19 11:02:49 +0100

    update main loop
    Change-Id: Ia864f3de37d3cad57e9335bf63aaabbeb028fbf1

--- src/train_few_shot.py
+++ src/train_few_shot.py
@@ -5,14 +5,11 @@ import torch.nn as nn
 import os
 import torchvision.transforms.functional as TF
 from tqdm import tqdm
-from few_shot_learning import (
-    FewShotDataSet,
-    FewShotSampler,
-    Omniglot,
-    RelationNet,
-    RelationNetAdaptater,
-)
-from few_shot_learning import TrainerFewShot, RotationTransform
+
+from  few_shot_learning import FewShotDataSet, FewShotSampler
+from  few_shot_learning.datasets import TrafficSignDataset,get_file_name_from_folder
+from  few_shot_learning.utils_train import TrainerFewShot,RotationTransform,get_n_trainable_param
+from few_shot_learning.relation_net import get_relation_net_adaptater
 
 import argparse
 
@@ -27,13 +24,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", default=5, type=int)
     parser.add_argument("-k", default=5, type=int)
-    parser.add_argument("-q", default=15, type=int)
-    parser.add_argument("--nb_ep", default=10, type=int)
+    parser.add_argument("-q", default=1, type=int)
+    parser.add_argument("--nb_ep", default=1, type=int)
     parser.add_argument("--nb_epochs", default=10, type=int)
-    parser.add_argument("--nb_eval", default=20, type=int)
+    parser.add_argument("--nb_eval", default=10, type=int)
     parser.add_argument("-lr", default=1e-4, type=float)
     parser.add_argument("--nb_of_batch", default=10, type=int)
     parser.add_argument("--step_size", default=100, type=int)
+    parser.add_argument("--model",default="RelationNet",type=str)
 
     args = parser.parse_args()
 
@@ -44,47 +42,36 @@ if __name__ == "__main__":
     number_of_batch = args.nb_of_batch
     step_size = args.step_size
 
-    transform = torchvision.transforms.Compose(
-        [
-            RotationTransform(angles=[90, 180, 270], fill=255),
-            torchvision.transforms.Resize(28, interpolation=2),
-            torchvision.transforms.ToTensor(),
-        ]
+    # ## load data
+
+    class TrafficSignDatasetWrapper(TrafficSignDataset):
+
+        def __init__(self,*args,**kwargs):
+            super(TrafficSignDatasetWrapper,self).__init__(*args,**kwargs)
+
+        def __getitem__(self,idx):
+            data = super(TrafficSignDatasetWrapper,self).__getitem__(idx)
+            return data["img"],data["label"]
+
+    transform = torchvision.transforms.Compose([
+                                            torchvision.transforms.Resize(256),
+                                            torchvision.transforms.RandomCrop(224),
+                                            torchvision.transforms.ToTensor(),
+                                            torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                 std=[0.229, 0.224, 0.225])
+                                           ])
+
+    list_file_name_train = get_file_name_from_folder(root_dir="/staging/thesis_data_search/data/patches",exclude_class = torch.arange(201,313))
+    list_file_name_val = get_file_name_from_folder(root_dir="/staging/thesis_data_search/data/patches",exclude_class = torch.arange(200))
+
+
+    bg_dataset = TrafficSignDatasetWrapper(
+        list_file_name_train,transform = transform
     )
 
-    transform_eval = torchvision.transforms.Compose(
-        [
-            torchvision.transforms.Resize(28),
-            torchvision.transforms.ToTensor(),
-        ]
+    eval_dataset = TrafficSignDatasetWrapper(
+        list_file_name_val,transform = transform
     )
-
-    transform = torchvision.transforms.Compose(
-        [
-            # torchvision.transforms.RandomRotation(90,expand=True),
-            # torchvision.transforms.RandomRotation(180,expand=True),
-            # torchvision.transforms.RandomRotation(270,expand=True),
-            torchvision.transforms.Resize(28),
-            torchvision.transforms.ToTensor(),
-        ]
-    )
-
-    bg_dataset = Omniglot(
-        root="/staging/thesis_data_search/data",
-        download=False,
-        transform=transform,
-        background=True,
-    )
-
-    eval_dataset = Omniglot(
-        root="/staging/thesis_data_search/data",
-        download=False,
-        transform=transform_eval,
-        background=False,
-    )
-
-    # ## Train
-
     # In[12]:
 
     few_shot_sampler = FewShotSampler(
@@ -117,16 +104,17 @@ if __name__ == "__main__":
 
     # In[26]:
 
-    loss_func = nn.MSELoss()
-    model = RelationNet(in_channels=1, out_channels=64, device=device, debug=True)
-    model.to(device)
+    if args.model == "RelationNet":
+        model_adaptater,model = get_relation_net_adaptater(nb_ep,n,k,q,device)
+    else :
+        raise NotImplementedError
 
     lr = args.lr
 
     optim = torch.optim.Adam(model.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=step_size, gamma=0.1)
 
-    model_adaptater = RelationNetAdaptater(model, loss_func, nb_ep, n, k, q, device)
+   
     trainer = TrainerFewShot(model_adaptater, device)
 
     epochs = args.nb_epochs
@@ -134,11 +122,11 @@ if __name__ == "__main__":
 
     trainer.fit(epochs, nb_eval, optim, scheduler, bg_taskloader, eval_taskloader)
 
-    min(trainer.list_loss_eval), min(trainer.list_loss), max(trainer.accuracy_eval)
+    print(min(trainer.list_loss_eval), min(trainer.list_loss), max(trainer.accuracy_eval))
 
     few_shot_accuracy_sampler = FewShotSampler(
         bg_dataset,
-        number_of_batch=100,
+        number_of_batch=1,
         episodes=nb_ep,
         sample_per_class=n,
         classes_per_ep=k,
