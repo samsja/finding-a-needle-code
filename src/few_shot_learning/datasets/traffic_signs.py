e685152a7927ec69af58881f3de1cfeaa5d77f95
Sami Jaghouar <sami.jaghouar@zenseact.com>
2021-03-26 13:42:40 +0100

    fix traffic sign for sampling
    Change-Id: If599975387c6f5fede879b09f502f5b1f30da790

--- src/few_shot_learning/datasets/traffic_signs.py
+++ src/few_shot_learning/datasets/traffic_signs.py
@@ -54,7 +54,9 @@ class TrafficSignDataset(FewShotDataSet):
                 self.data.append(f"{root_dir}/{fn}")
 
         self.update_classes_indexes()
-        self._classes = torch.tensor(self.labels).unique()
+        
+        self._unique_classes = torch.tensor(self.labels).unique()
+        self._classes = torch.arange(self._unique_classes.size(0))
 
         self.transform = transform
 
@@ -72,6 +74,7 @@ class TrafficSignDataset(FewShotDataSet):
 
     def get_index_in_class(self, class_idx):
         """
+        DEPRECATED
         Method to get the indexes of the elements in the same class as class_idx
 
         # Args:
@@ -97,7 +100,7 @@ class TrafficSignDataset(FewShotDataSet):
 
         """
 
-        return self.classes_indexes[class_idx]
+        return self.classes_indexes[self._unique_classes[class_idx]]
 
     def get_index_in_class_vect(self, class_idx_vect: torch.Tensor):
         """
@@ -175,8 +178,9 @@ class TrafficSignDataset(FewShotDataSet):
 
         self.data.insert(i, file_name)
         self.labels.insert(i, c_idx)
-
-        self._classes = torch.tensor(self.labels).unique()
+ 
+        self._unique_classes = torch.tensor(self.labels).unique()
+        self._classes = torch.arange(self._unique_classes.size(0))
 
     def remove_datapoints(self, ids):
         """
@@ -190,8 +194,9 @@ class TrafficSignDataset(FewShotDataSet):
         self.data = [i for  j, i in enumerate(self.data) if j not in ids]
 
         self.labels = [i for  j, i in enumerate(self.labels) if j not in ids]
-
-        self._classes = torch.tensor(self.labels).unique()
+ 
+        self._unique_classes = torch.tensor(self.labels).unique()
+        self._classes = torch.arange(self._unique_classes.size(0))
 
 from typing import Dict, List
 
