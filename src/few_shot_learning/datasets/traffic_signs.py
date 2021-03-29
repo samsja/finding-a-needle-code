fix error on task loader for Traffic sign
Change-Id: I3ed09972428af0f8979fb8ca3be6752a8e007a10

src/few_shot_learning/datasets/traffic_signs.py
@@ -126,7 +126,7 @@ class TrafficSignDataset(FewShotDataSet):
         x = self.transform(x)
 
         if not(type(idx)==torch.Tensor):
-            idx = torch.Tensor(idx)
+            idx = torch.Tensor([idx]).clone().detach()
 
         data = {"img": x, 
                 "label": torch.tensor(y), 