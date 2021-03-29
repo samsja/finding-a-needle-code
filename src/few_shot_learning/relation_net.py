fix error on task loader for Traffic sign
Change-Id: Ibb5393d425d139353236ec2cd8cc4d1421bf7902
Reviewed-on: https://gerrit.cicd.autoheim.net/c/thesis_data_search/+/233223
Reviewed-by: Hannes Gustafsson <hannes.gustafsson@zenseact.com>
Tested-by: Hannes Gustafsson <hannes.gustafsson@zenseact.com>

src/few_shot_learning/relation_net.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from .utils_train import ModuleAdaptater
 from torchvision.models import resnet18
 import copy
-
+from tqdm import tqdm
 
 def get_conv_block_mp(
     in_channels: int, out_channels: int, padding: int = 0
@@ -366,7 +366,7 @@ class RelationNetAdaptater(ModuleAdaptater):
 
         relations = []
         index = []
-        for idx, batch in enumerate(test_taskloader):
+        for idx, batch in enumerate(tqdm(test_taskloader)):
 
             query_inputs = batch["img"].to(self.device)
 