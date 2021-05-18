17a4f907c684d12f81ae0efa7e6e6d37f2a2cd0e
Sami Jaghouar <sami.jaghouar@zenseact.com>
2021-05-18 14:11:53 +0200

    refacto toy example
    Change-Id: I0f4a78d457577025c48be4e81cce07e34aee5b0c

--- src/toy_exemple.py
+++ src/toy_exemple.py
@@ -5,16 +5,14 @@ import numpy as np
 from sklearn.datasets import make_blobs, make_moons
 import matplotlib.pyplot as plt
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-def train_and_vizu(X, y, lr, epochs):
+def train_and_vizu(X, y, lr, epochs,balanced_loss,device):
     data = torch.from_numpy(X).float().to(device)
     labels = torch.from_numpy(y).long().to(device)
 
-    model, acc = train(data, labels, lr, epochs)
+    model, acc = train(data, labels, device,lr, epochs,balanced_loss)
     print(acc)
 
-    vizu(model, X, y)
+    vizu(model, X, y,device)
 
     return model
 
@@ -33,26 +31,17 @@ def make_blobs_few_shot(X, y, ratio):
     return X_few, y_few
 
 
-def train(data,labels,lr=1e-2, epochs=100):
+def train(data,labels,device,lr=1e-2, epochs=100,balanced_loss=False,callback=None):
     model = nn.Sequential(
         nn.Linear(2, len(labels.unique())),
     )
 
     model = model.to(device)
-    
-    return continue_train(model,data, labels, lr=1e-2, epochs=100)
-
-def continue_train(model,data, labels, lr=1e-2, epochs=100):
-    loss_fn = nn.CrossEntropyLoss()
-    optim = torch.optim.Adam(model.parameters(), lr=lr)
-    
-    for _ in range(epochs):
-
-        model.zero_grad()
-        outputs = model(data)
-        loss = loss_fn(outputs, labels)
-        loss.backward()
-        optim.step()
+  
+    if balanced_loss:
+        model = train_custom_loss(model,data, labels, lr, epochs,callback)
+    else:
+        model = train_normal_loss(model,data, labels, lr, epochs,callback)
 
     with torch.no_grad():
 
@@ -60,10 +49,51 @@ def continue_train(model,data, labels, lr=1e-2, epochs=100):
 
         acc = (outputs == labels).float().mean()
 
-    return model, acc
 
-def vizu(model, X, y, figsize=(17, 7)):
-    h = 0.25
+    return model,acc
+
+def train_custom_loss(model,data, labels, device, lr=1e-2, epochs=100,callback=None):
+    loss_fn = nn.CrossEntropyLoss()
+    optim = torch.optim.Adam(model.parameters(), lr=lr)
+    
+
+    for _ in range(epochs):
+
+        model.zero_grad()
+        outputs = model(data)
+        loss = 0
+        
+        distrib = labels.bincount()
+        
+        for class_,count in enumerate(distrib):
+            
+            loss += loss_fn(outputs[labels==class_], labels[labels==class_])/count
+
+        loss.backward()
+        optim.step()
+
+    return model
+
+
+def train_normal_loss(model,data, labels, lr=1e-2, epochs=100,callback=None):
+    loss_fn = nn.CrossEntropyLoss()
+    optim = torch.optim.Adam(model.parameters(), lr=lr)
+    
+    for idx_epoch in range(epochs):
+
+        model.zero_grad()
+        outputs = model(data)
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+        optim.step()
+
+        if callback is not None:
+            callback(model=model,data=data,labels=labels,idx_epoch=idx_epoch)
+ 
+    return model
+
+def vizu(model, X, y, device,figsize=(17, 7)):
+    h = 0.05
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
@@ -79,3 +109,12 @@ def vizu(model, X, y, figsize=(17, 7)):
 
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
+
+
+def boxplot_proba_few_shot(model,X,y,device):
+   
+    with torch.no_grad():
+        outputs = model(X).softmax(dim=1)
+    
+    plt.boxplot([outputs[y == 0][:, 0].to("cpu"), outputs[y != 0][:, 0].to("cpu")])
+    plt.title("Probability that point belongs to the class with few shot")
