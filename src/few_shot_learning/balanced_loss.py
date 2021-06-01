1243fbb4140286bc7750ca24aaf6b13fca0b24c5
Sami Jaghouar <sami.jaghouar@zenseact.com>
2021-06-01 13:52:12 +0200

    add balanced loss
    Change-Id: Ib84658d05b72d322eed4aa68c91b3275e39935f2

---
+++ src/few_shot_learning/balanced_loss.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+
+def balanced_loss(outputs,labels):
+
+    distrib = labels.bincount()
+
+    loss_fn = nn.CrossEntropyLoss()
+
+    loss = torch.tensor(0).float().to(outputs.device)
+    loss.requires_grad = True
+
+
+    for class_, count in enumerate(distrib):
+
+        
+        loss_class = loss_fn(outputs[labels == class_], labels[labels == class_])* ( sum(distrib)/ count) if count != 0 else torch.tensor(0).float().to(outputs.device)
+
+        loss = loss + loss_class
+        
+        return loss
